aa2f7a88c3821a23900ae4f7d0ab45ddbfc8191c
Ukesh Dhakal <130669170+ukesh-dhakal@users.noreply.github.com>
2025-07-02 12:14:28 +0545

    Update Projects.tsx

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -61,15 +61,22 @@ const Projects = () => {
     {
       id: 1,
       title: 'HTML CSS and JS Live Code Editor',
-      description: 'A live code editor built using Vanilla HTML, JavaScript, and Tailwind CSS, allowing real-time preview of web development code.',
+      description: 'A live code editor built using Vanilla HTML, JavaScript, and Tailwind CSS, allowing real-time preview of web development code.
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hammer" viewBox="0 0 16 16">
+  <path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5 5 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334"/>
+</svg>
+        ',
       technologies: ['html', 'css', 'javascript', 'tailwind'],
       
-      liveLink: 'https://ukesh-dhakal.github.io/LivecodeEditor-For-HTML-CSS-and-JS/',
+      liveLink: 'https://github.com',
     },
     {
       id: 2,
       title: 'Event Management System',
-      description: 'A full-stack web application for event management, developed with React, Tailwind CSS, Node.js, and MongoDB.',
+      description: 'A full-stack web application for event management, developed with React, Tailwind CSS, Node.js, and MongoDB.
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-hammer" viewBox="0 0 16 16">
+  <path d="M9.972 2.508a.5.5 0 0 0-.16-.556l-.178-.129a5 5 0 0 0-2.076-.783C6.215.862 4.504 1.229 2.84 3.133H1.786a.5.5 0 0 0-.354.147L.146 4.567a.5.5 0 0 0 0 .706l2.571 2.579a.5.5 0 0 0 .708 0l1.286-1.29a.5.5 0 0 0 .146-.353V5.57l8.387 8.873A.5.5 0 0 0 14 14.5l1.5-1.5a.5.5 0 0 0 .017-.689l-9.129-8.63c.747-.456 1.772-.839 3.112-.839a.5.5 0 0 0 .472-.334"/>
+</svg> ',
       technologies: ['react', 'tailwind', 'nodejs', 'mongodb'],
       liveLink: 'https://your-event-management-demo.com',
     },
